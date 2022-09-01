0a469595935c659bacf779ce3c411048664ddad5
Mudassir742 <mudassirwaheed242@gmail.com>
2022-09-01 18:01:57 +0500

    images added in service cards

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,12 +1,17 @@
 import React from "react";
 import {
   CardSection,
+  Image,
   SectionHeading,
   ServiceCard,
   ServiceDetail,
   ServiceHeading,
   ServiceSection,
 } from "./Services.styled";
+
+import FrontendImage from "../../asset/icons/frontend.png";
+import BackendImage from "../../asset/icons/backend.png";
+import BugImage from "../../asset/icons/bug.png";
 
 //import "./Services.css";
 const Services = () => {
@@ -15,6 +20,7 @@ const Services = () => {
       <SectionHeading>My Services</SectionHeading>
       <CardSection>
         <ServiceCard>
+          <Image src={FrontendImage} alt="icon" />
           <ServiceHeading>Frontend</ServiceHeading>
           <ServiceDetail>
             I am very good in replicating designs.I have design many web pages
@@ -22,8 +28,24 @@ const Services = () => {
             of design to responsive web pages.
           </ServiceDetail>
         </ServiceCard>
-        <ServiceCard>1</ServiceCard>
-        <ServiceCard>1</ServiceCard>
+        <ServiceCard>
+          <Image src={BackendImage} alt="icon" />
+          <ServiceHeading>Backend</ServiceHeading>
+          <ServiceDetail>
+            I am very good in replicating designs.I have design many web pages
+            with amaizing design. I will convert your static figma or any type
+            of design to responsive web pages.
+          </ServiceDetail>
+        </ServiceCard>
+        <ServiceCard>
+          <Image src={BugImage} alt="icon" />
+          <ServiceHeading>Bug Fixing</ServiceHeading>
+          <ServiceDetail>
+            I am very good in replicating designs.I have design many web pages
+            with amaizing design. I will convert your static figma or any type
+            of design to responsive web pages.
+          </ServiceDetail>
+        </ServiceCard>
       </CardSection>
     </ServiceSection>
   );
--- src/components/services/Services.styled.js
+++ src/components/services/Services.styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const ServiceSection = styled.section`
-width:100vw;
-padding:2rem 5rem 5rem 5rem;
-max-width:100%;
-background:${({theme})=>theme.colors.primary.darker}
-`
+  width: 100vw;
+  padding: 2rem 5rem 5rem 5rem;
+  max-width: 100%;
+  background: ${({ theme }) => theme.colors.primary.darker};
+`;
 
 export const SectionHeading = styled.p`
   width: 100%;
@@ -24,31 +24,39 @@ export const SectionHeading = styled.p`
 `;
 
 export const CardSection = styled.div`
-display: grid;
-grid-template-columns: repeat(3, 1fr);
-grid-gap: 40px;
-`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 40px;
+`;
 
 export const ServiceCard = styled.div`
-display:flex;
-flex-direction:column;
-align-items:center;
-background:${({theme})=>theme.colors.primary.dark};
-color:white;
-padding:1.5rem 2rem;
-border-radius:10px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.primary.dark};
+  color: white;
+  padding: 1.5rem 2rem;
+  border-radius: 10px;
+`;
+
+export const Image = styled.img`
+  width: 70px;
+  height: auto;
+  @media (max-width: 872px) {
+    width: 40px;
+  }
+`;
 
 export const ServiceHeading = styled.p`
-font-size:1.8rem;
-font-weight:500;
-letter-spacing:1px;
-
-`
+  font-size: 1.5rem;
+  margin: 1.5rem 0 0.5rem 0;
+  font-weight: 500;
+  letter-spacing: 1px;
+`;
 export const ServiceDetail = styled.p`
-text-align :center;
-margin:1rem 0;
-line-height:1.5rem;
-letter-spacing:1px;
-font-size:1rem;
-`
+  text-align: center;
+  margin: 1rem 0;
+  line-height: 1.5rem;
+  letter-spacing: 1px;
+  font-size: 0.9rem;
+`;
