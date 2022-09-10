d975bcb0272e01d2a2804a465690abb80600ce52
Mudassir742 <mudassirwaheed242@gmail.com>
2022-09-10 23:54:15 +0500

    techcard tested completed

--- src/components/technology/Technology.js
+++ src/components/technology/Technology.js
@@ -1,15 +1,11 @@
 import React from "react";
 import {
-  Image,
   SectionHeading,
-  TechCard,
   TechContainer,
-  TechContent,
-  TechDetail,
-  TechHeading,
-  TechInfo,
   TechnologySection,
 } from "./Technology.styled";
+
+import TechnologyCard from "./TechnologyCard";
 
 import ReactImage from "../../asset/reactjs.png";
 import NodeImage from "../../asset/nodejs.png";
@@ -24,103 +20,56 @@ const Technology = () => {
     <TechnologySection>
       <SectionHeading>Technology Stack</SectionHeading>
       <TechContainer>
-        <TechCard>
-          <TechContent>
-            <Image src={ReactImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>Reactjs</TechHeading>
-              <TechDetail>
-                ReactJS is a declarative, efficient, and flexible JavaScript
-                library for building reusable UI components.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={ReactImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>React Native</TechHeading>
-              <TechDetail>
-                React Native is also a javascript library use to develope
-                cross-platform mobile application with flexibilty of UI
-                components.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={NextImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>Nextjs</TechHeading>
-              <TechDetail>
-                Nextjs is web development framework created by Vercel enabling
-                React-based web applications with server-side rendering
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={NodeImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>Nodejs</TechHeading>
-              <TechDetail>
-                Node.js is an open-source and cross-platform JavaScript runtime
-                environment. It is a popular tool for running Javascript.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={ExpressImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>Expressjs</TechHeading>
-              <TechDetail>
-                Express is a Node.js web application framework that provides a
-                robust set of features for web and mobile applications.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={MongoImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>MongoDB</TechHeading>
-              <TechDetail>
-                MongoDB is a document-oriented database program. Classified as a
-                NoSQL database program, and uses JSON-like documents.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={SqlImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>My SQL</TechHeading>
-              <TechDetail>
-                MySQL is a relational database management system. It enables
-                users to meet the database challenges scalability.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
-        <TechCard>
-          <TechContent>
-            <Image src={PostmanImage} alt="logo" />
-            <TechInfo>
-              <TechHeading>Postman</TechHeading>
-              <TechDetail>
-                Postman is an application that allows us to mock up frontend
-                requests without writing any JavaScript.
-              </TechDetail>
-            </TechInfo>
-          </TechContent>
-        </TechCard>
+        <TechnologyCard
+          image={ReactImage}
+          techName="Reactjs"
+          techDetail="ReactJS is a declarative, efficient, and flexible JavaScript
+                library for building reusable UI components."
+        />
+        <TechnologyCard
+          image={ReactImage}
+          techName="React Native"
+          techDetail="React Native is also a javascript library use to develope
+          cross-platform mobile application with flexibilty of UI
+          components."
+        />
+        <TechnologyCard
+          image={NextImage}
+          techName="Nextjs"
+          techDetail="Nextjs is web development framework created by Vercel enabling
+          React-based web applications with server-side rendering."
+        />
+        <TechnologyCard
+          image={NodeImage}
+          techName="Nodejs"
+          techDetail="Node.js is an open-source and cross-platform JavaScript runtime
+          environment. It is a popular tool for running Javascript."
+        />
+        <TechnologyCard
+          image={ExpressImage}
+          techName="Expressjs"
+          techDetail="Express is a Node.js web application framework that provides a
+          robust set of features for web and mobile applications."
+        />
+
+        <TechnologyCard
+          image={MongoImage}
+          techName="MongoDB"
+          techDetail="MongoDB is a document-oriented database program. Classified as a
+          NoSQL database program, and uses JSON-like documents."
+        />
+        <TechnologyCard
+          image={SqlImage}
+          techName="My SQL"
+          techDetail="MySQL is a relational database management system. It enables
+          users to meet the database challenges scalability."
+        />
+        <TechnologyCard
+          image={PostmanImage}
+          techName="Postman"
+          techDetail="Postman is an application that allows us to mock up frontend
+          requests without writing any JavaScript."
+        />
       </TechContainer>
     </TechnologySection>
   );
