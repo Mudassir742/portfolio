b4ef695efb250039f8b6c5882a328eeee1fbb70b
Mudassir742 <mudassirwaheed242@gmail.com>
2022-09-11 22:50:51 +0500

    testimonial cards created

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -5,6 +5,8 @@ import {
   TestimonialCard,
   RearCard,
   FrontCard,
+  Feedback,
+  ClientName,
 } from "./Testimonials.styled";
 
 const Testimonials = () => {
@@ -13,10 +15,28 @@ const Testimonials = () => {
       <SectionHeading>Testimonials</SectionHeading>
       <TestimonialContainer>
         <TestimonialCard>
-          <RearCard></RearCard>
           <FrontCard>
-            
+            <Feedback>
+              Chess Money is a joint venture between a cohort of ex-bankers and
+              a listed company that provides human resources management
+              services. Each of the Chess partners’ competencies is leveraged to
+              offer the best lending experience.
+            </Feedback>
+            <ClientName>John Doe.</ClientName>
           </FrontCard>
+          <RearCard></RearCard>
+        </TestimonialCard>
+        <TestimonialCard>
+          <FrontCard>
+            <Feedback>
+              Chess Money is a joint venture between a cohort of ex-bankers and
+              a listed company that provides human resources management
+              services. Each of the Chess partners’ competencies is leveraged to
+              offer the best lending experience.
+            </Feedback>
+            <ClientName>John Doe.</ClientName>
+          </FrontCard>
+          <RearCard></RearCard>
         </TestimonialCard>
       </TestimonialContainer>
     </TestimonialSection>
--- src/components/testimonials/Testimonials.styled.js
+++ src/components/testimonials/Testimonials.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const TestimonialSection = styled.section`
   width: 100vw;
+  position: relative;
   padding: 2rem 5rem 5rem 5rem;
   max-width: 100%;
   background: ${({ theme }) => theme.colors.primary.darker};
@@ -12,7 +13,7 @@ export const TestimonialSection = styled.section`
 export const SectionHeading = styled.p`
   width: 100%;
   text-align: center;
-  margin: 2rem 0 4rem 0;
+  margin: 2rem 0 6rem 0;
   font-size: 3rem;
   color: ${({ theme }) => theme.colors.primary.lighter};
   letter-spacing: 1px;
@@ -26,24 +27,57 @@ export const SectionHeading = styled.p`
 `;
 
 export const TestimonialContainer = styled.div`
-display: grid;
-grid-template-columns: repeat(3, 1fr);
-grid-gap: 40px;
-@media (max-width: 930px) {
-  grid-gap: 30px;
-}
-@media (max-width: 860px) {
-  grid-template-columns: repeat(1, 1fr);
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(2, 1fr);
   grid-gap: 45px;
-}
+  @media (max-width: 930px) {
+    grid-gap: 30px;
+    grid-template-columns: repeat(1, 1fr);
+  }
+  @media (max-width: 860px) {
+    grid-gap: 45px;
+  }
 `;
 export const TestimonialCard = styled.div`
   position: relative;
+  @media (max-width: 930px) {
+    margin-bottom: 3rem;
+  }
 `;
 export const FrontCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.primary.dark};
+  color: ${({ theme }) => theme.colors.primary.lighter};
+  border-radius: 6px;
+  padding: 2.5rem 2.5rem 2rem 2.5rem;
+  position: relative;
+  z-index: 1;
 
+  @media (max-width: 860px) {
+    padding: 1.8rem 2.5rem;
+  }
+  @media (max-width: 600px) {
+    padding: 1.6rem 2.2rem;
+  }
+  @media (max-width: 390px) {
+    padding: 1.4rem 1.8rem;
+  }
 `;
-export const RearCard = styled.div``;
+export const RearCard = styled.div`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  position: absolute;
+  background: white;
+  border-radius: 6px;
+  transform: rotate(-10deg);
+  @media (max-width: 930px) {
+    transform: rotate(-6deg);
+  }
+`;
 
 export const QoutationMark = styled.div``;
 
